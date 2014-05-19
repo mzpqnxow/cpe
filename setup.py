2bad21432e8dedd0c3325209fb67b90dc3c4c12b
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2014-05-20 00:44:21 +0200

    Vagrant fixes and python2.6 requirements (ordereddict).

--- setup.py
+++ setup.py
@@ -5,6 +5,19 @@ from __future__ import absolute_import, unicode_literals
 from io import open
 import os
 import setuptools
+import sys
+
+
+# Delete os.link if we are using Vagrant, because tox requires a file
+# system with hard links to work.
+#
+# The hard link only saves a little disk space, so you should not care
+if os.environ.get('USER','') == 'vagrant':
+    try:
+        del os.link
+    except AttributeError:
+        pass
+
 
 BASE_DIR = os.path.dirname(__file__)
 PKG_DIR = os.path.join(BASE_DIR, "cpe")
@@ -15,6 +28,11 @@ with open(os.path.join(PKG_DIR, "__meta__.py"), 'rb') as f:
 
 with open(os.path.join(BASE_DIR, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
+
+install_requires = []
+
+if sys.version_info[:2] < (2, 7):
+    install_requires.append('ordereddict')
 
 
 setuptools.setup(
@@ -40,5 +58,6 @@ setuptools.setup(
     license=meta["__license__"],
     url=meta["__url__"],
     include_package_data=False,
-    packages=setuptools.find_packages(exclude=['tests', 'docs']),
+    install_requires=install_requires,
+    packages=(b'cpe', b'cpe.comp'),
 )
